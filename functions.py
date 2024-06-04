d11341aa8e7af97219dbfa95bb1946739193fd32
Sixtus Aondoakaa <sixtusushrey@gmail.com>
2024-06-04 22:27:24 +0100

    first commit on ths file

---
+++ functions.py
@@ -0,0 +1,35 @@
+def greet():
+    print("Hello, welcome to Python!")
+
+
+greet()
+
+
+
+
+# Challenge 1: Create a function that takes a name as an argument and prints "Hello, [name]":
+def greet(name):
+    print(f"Hello, {name}! Welcome to Python!")
+    return name
+
+
+
+def greet(name="Guest"):
+    print(f"Hello, {name}! Welcome to Python!")
+
+
+greet("Alice")  # Output: Hello, Alice! Welcome to Python!
+greet()         # Output: Hello, Guest! Welcome to Python!
+
+
+def print_numbers(*args):
+    for number in args:
+        print(number)
+
+print_numbers(1, 2, 3, 4)  # Output: 1 2 3 4
+
+def print_key_values(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+print_key_values(a=1, b=2, c=3)  # Output: a: 1 b: 2 c: 3
