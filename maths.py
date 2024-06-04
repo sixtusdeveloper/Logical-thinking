c674c034ace8b0ab37ec447e87342ed3dbcb8a0b
Sixtus Aondoakaa <sixtusushrey@gmail.com>
2024-06-04 22:35:05 +0100

    commit on this file

--- maths.py
+++ maths.py
@@ -17,3 +17,30 @@ print(b)
 
 c = 48565878 * 578453
 print(c)
+
+
+
+
+z = 5
+
+def modify_global():
+    global z
+    z = 10
+
+modify_global()
+print(z)  # Output: 10
+
+
+
+def outer():
+    a = 5
+
+    def inner():
+        nonlocal a
+        a = 10
+        print(a)  # Output: 10
+
+    inner()
+    print(a)  # Output: 10
+
+outer()
