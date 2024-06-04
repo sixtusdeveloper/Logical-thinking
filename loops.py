03b6d4a1bc88faf3e16a6c7fc722f992fd7436b1
Sixtus Aondoakaa <sixtusushrey@gmail.com>
2024-06-04 22:41:32 +0100

    commit  on this file

--- loops.py
+++ loops.py
@@ -89,3 +89,12 @@ else:
 
 lst = [1, 2, 3]
 lst.append(4)  # lst is now [1, 2, 3, 4]
+
+
+lst = [1, 2, 3]
+lst.insert(1, 1.5)  # lst is now [1, 1.5, 2, 3]
+
+
+
+lst = [1, 2, 3, 2]
+lst.remove(2)  # lst is now [1, 3, 2]
