9c9ba3c8d081117b93685993a2aa90ee3be156fc
Sixtus Aondoakaa <sixtusushrey@gmail.com>
2024-06-04 22:37:00 +0100

    commit on file

--- loops.py
+++ loops.py
@@ -23,3 +23,29 @@ count = 0
 while count < 5:
     print(count)
     count += 1
+
+
+# Challenge 5: Print each fruit in a fruit list, using a for loop:
+user_input = ""
+while user_input != "exit":
+    user_input = input("Type 'exit' to end the loop: ")
+    print("You typed:", user_input)
+    print("Type 'exit' to end the loop.")
+
+
+# Challenge 6: Print each fruit in a fruit list, using a for loop:
+
+
+i = 1
+while i <= 10:
+    if i == 5:
+        break
+    print(i)
+    i += 1
+
+
+
+for i in range(1, 11):
+    if i == 5:
+        break
+    print(i)
