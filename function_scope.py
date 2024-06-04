dc7a9a3501cb9720c5cf4174a145119bba69a760
Sixtus Aondoakaa <sixtusushrey@gmail.com>
2024-06-04 22:32:14 +0100

    commit on a new file

--- function_scope.py
+++ function_scope.py
@@ -10,3 +10,27 @@ def outer_function():
     print(x)  # Output: 20
 
 outer_function()
+
+
+def my_function():
+    x = 10  # x is a local variable
+    print(x)
+
+my_function()  # Output: 10
+print(x)  # This will raise a NameError since x is not accessible outside the function
+
+
+
+
+def outer_function():
+    x = 10  # x is in the enclosing scope
+
+    def inner_function():
+        nonlocal x
+        x = 20  # Modifies the x in the enclosing scope
+        print(x)
+
+    inner_function()  # Output: 20
+    print(x)  # Output: 20
+
+outer_function()
